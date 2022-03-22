386598e6264e5fc2f8b42322d0521a103ef2ab11
Thanos <tstratiko@gmail.com>
2022-03-22 22:05:53 +0200

    [wip] License header added

--- drivers/drivers-common/src/main/java/uk/ac/manchester/tornado/drivers/common/TornadoVMCompiler.java
+++ drivers/drivers-common/src/main/java/uk/ac/manchester/tornado/drivers/common/TornadoVMCompiler.java
@@ -1,8 +1,29 @@
+/*
+ * Copyright (c) 2022, APT Group, Department of Computer Science,
+ * School of Engineering, The University of Manchester. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * Copyright (c) 2009, 2017, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package uk.ac.manchester.tornado.drivers.common;
 
-import jdk.vm.ci.code.CompilationRequest;
 import jdk.vm.ci.meta.ProfilingInfo;
 import jdk.vm.ci.meta.ResolvedJavaMethod;
+import org.graalvm.compiler.code.CompilationResult;
 import org.graalvm.compiler.core.target.Backend;
 import org.graalvm.compiler.lir.asm.CompilationResultBuilderFactory;
 import org.graalvm.compiler.lir.phases.LIRSuites;
@@ -18,10 +39,21 @@ public class TornadoVMCompiler {
 
     }
 
-    public static <T extends CompilationRequest> void compileGraph(StructuredGraph graph, ResolvedJavaMethod installedCodeOwner, Providers providers, Backend backend,
+    /**
+     * Requests compilation of a given graph.
+     *
+     * @param graph
+     *            the graph to be compiled
+     * @param installedCodeOwner
+     *            the method the compiled code will be associated with once
+     *            installed. This argument can be null.
+     * @return the result of the compilation
+     */
+    public static <T extends CompilationResult> T compileGraph(StructuredGraph graph, ResolvedJavaMethod installedCodeOwner, Providers providers, Backend backend,
             PhaseSuite<HighTierContext> graphBuilderSuite, OptimisticOptimizations optimisticOpts, ProfilingInfo profilingInfo, Suites suites, LIRSuites lirSuites, T compilationResult,
             CompilationResultBuilderFactory factory, boolean verifySourcePositions) {
         System.out.println("--- TornadoVM - compileGraph signature");
+        return null;
     }
 
 }
