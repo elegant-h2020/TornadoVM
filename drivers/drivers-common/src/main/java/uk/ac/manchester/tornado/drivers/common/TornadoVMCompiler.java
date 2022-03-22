b76178615cc322d3a10af809c654d7995bb587f5
Thanos <tstratiko@gmail.com>
2022-03-22 21:44:09 +0200

    [wip] Updated signature of compileGraph method

--- drivers/drivers-common/src/main/java/uk/ac/manchester/tornado/drivers/common/TornadoVMCompiler.java
+++ drivers/drivers-common/src/main/java/uk/ac/manchester/tornado/drivers/common/TornadoVMCompiler.java
@@ -13,38 +13,15 @@ import org.graalvm.compiler.phases.tiers.HighTierContext;
 import org.graalvm.compiler.phases.tiers.Suites;
 import org.graalvm.compiler.phases.util.Providers;
 
-public class TornadoVMCompiler<T extends CompilationRequest> {
+public class TornadoVMCompiler {
     public TornadoVMCompiler() {
 
     }
 
-    // public static void compileGraph(StructuredGraph graph, ResolvedJavaMethod
-    // installedCodeOwner, Providers providers, Backend backend) {
-    // System.out.println("--- TornadoVM - new compileGraph");
-    // }
-
-    public void compileGraph(StructuredGraph graph, ResolvedJavaMethod installedCodeOwner, Providers providers, Backend backend, PhaseSuite<HighTierContext> graphBuilderSuite,
-            OptimisticOptimizations optimisticOpts, ProfilingInfo profilingInfo, Suites suites, LIRSuites lirSuites, T compilationResult, CompilationResultBuilderFactory factory,
-            boolean verifySourcePositions) {
+    public static <T extends CompilationRequest> void compileGraph(StructuredGraph graph, ResolvedJavaMethod installedCodeOwner, Providers providers, Backend backend,
+            PhaseSuite<HighTierContext> graphBuilderSuite, OptimisticOptimizations optimisticOpts, ProfilingInfo profilingInfo, Suites suites, LIRSuites lirSuites, T compilationResult,
+            CompilationResultBuilderFactory factory, boolean verifySourcePositions) {
         System.out.println("--- TornadoVM - compileGraph signature");
-    }
-
-    public static void compileGraph2() {
-        // public static <T extends CompilationResult> T compileGraph(StructuredGraph
-        // graph, ResolvedJavaMethod installedCodeOwner, Providers providers, Backend
-        // backend, PhaseSuite<HighTierContext> graphBuilderSuite,
-        // OptimisticOptimizations optimisticOpts, ProfilingInfo profilingInfo, Suites
-        // suites, LIRSuites lirSuites, T compilationResult,
-        // CompilationResultBuilderFactory factory, boolean verifySourcePositions) {
-        // Request<OCLCompilationResult> r = new Request<>(graph, installedCodeOwner,
-        // null, null, providers, (OCLBackend) backend, graphBuilderSuite,
-        // optimisticOpts,
-        // profilingInfo, null, null, compilationResult, factory, true, true, 0);
-        // // TODO Suites, backend, lirSuites, CompilationResult
-        // CompilationResult result = r.execute();
-        // return (T) result;
-        System.out.println("--- TornadoVM - compileGraph2");
-        // return null;
     }
 
 }
